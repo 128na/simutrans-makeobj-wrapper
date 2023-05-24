4f603a8331048879d902566814f51177f12cf964
128na <simutrans.128na@gmail.com>
2023-05-24 18:20:07 +0900

    add type

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 
 import Makeobj from './src/Makeobj';
+import MakeobjAsync from './src/MakeobjAsync';
 import MakeobjResponse from './src/MakeobjResponse';
 
 
 export {
   Makeobj,
+  MakeobjAsync,
   MakeobjResponse
 }
